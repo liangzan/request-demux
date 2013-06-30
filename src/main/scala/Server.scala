f35847bed240b7ca4536e9fa5f411b547436e153
Wong Liang Zan <zan@liangzan.net>
2013-07-01 06:22:30 +0800

    removed the unnecessary code

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -3,18 +3,11 @@ package com.sproutmode
 import unfiltered.request._
 import unfiltered.response._
 
-import org.clapper.avsl.Logger
-
 /** embedded server */
 object Server {
-  val logger = Logger(Server.getClass)
+  val port = 3500
   def main(args: Array[String]) {
-    val http = unfiltered.jetty.Http.local(3500) // this will not be necessary in 0.4.0
-    http.context("/assets") { _.resources(new java.net.URL(getClass().getResource("/www/css"), ".")) }
-      .filter(new App).run({ svr =>
-        unfiltered.util.Browser.open(http.url)
-      }, { svr =>
-        logger.info("shutting down server")
-      })
+    val http = unfiltered.jetty.Http.local(port) // this will not be necessary in 0.4.0
+    http.filter(new App).run()
   }
 }
