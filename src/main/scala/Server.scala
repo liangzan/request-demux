a4ece29551bba2d26401a022c260ff3c97b3ebe3
Wong Liang Zan <zan@liangzan.net>
2013-07-01 05:42:04 +0800

    changed the port

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -9,7 +9,7 @@ import org.clapper.avsl.Logger
 object Server {
   val logger = Logger(Server.getClass)
   def main(args: Array[String]) {
-    val http = unfiltered.jetty.Http.anylocal // this will not be necessary in 0.4.0
+    val http = unfiltered.jetty.Http.local(3500) // this will not be necessary in 0.4.0
     http.context("/assets") { _.resources(new java.net.URL(getClass().getResource("/www/css"), ".")) }
       .filter(new App).run({ svr =>
         unfiltered.util.Browser.open(http.url)
