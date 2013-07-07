102fd01bdf6f5a58dde021c89e94e580cb0e8340
Wong Liang Zan <zan@liangzan.net>
2013-07-08 05:44:51 +0800

    switched to use scala logging

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,13 +3,11 @@ package com.sproutmode
 import unfiltered.request._
 import unfiltered.response._
 
-import org.clapper.avsl.Logger
+import com.typesafe.scalalogging.slf4j._
 
 /** unfiltered plan */
-class App extends unfiltered.filter.Plan {
+class App extends unfiltered.filter.Plan with Logging {
   import QParams._
-
-  val logger = Logger(classOf[App])
 
   def intent = {
     case GET(Path(p)) =>
@@ -17,7 +15,7 @@ class App extends unfiltered.filter.Plan {
       Ok ~> ResponseString("ok")
 
     case POST(Path(p) & Params(params)) =>
-      logger.info("POST %s" format p)
+      logger.info("POST %s [%s]" format (p, params))
       Ok ~> ResponseString("ok")
 
     case _ => Pass
